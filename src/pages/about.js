8f7ae5b891650f9438061367fe7d4322692a4aaf
ubitinaitis <bitinaitis.u@northeastern.edu>
2022-02-16 15:36:12 -0500

    gatsby tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// define component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+    <p>Hi! My name's Ula!</p>
+    </Layout>
+  )
+}
+// export component
+export default AboutPage
